d3fd191197f36d9d4d83a7139d2e6fc1744377a8
Jonatas <djonatas31@gmail.com>
2023-08-01 16:56:13 -0300

    update

--- 2projetoVue/vue-project/src/App.vue
+++ 2projetoVue/vue-project/src/App.vue
@@ -1,14 +1,19 @@
 <template>
-    <primeiroComponente/>
+    <div>
+        <primeiroComponente/>
+        <segundoComponente/>
+    </div>
 </template>
 
 <script>
     import primeiroComponente from './components/primeiroComponente.vue';
+import SegundoComponente from './components/segundoComponente.vue';
 
     export default {
         name: 'App',
         components: {
-            primeiroComponente
+            primeiroComponente,
+                SegundoComponente
         }
     }
 </script>
