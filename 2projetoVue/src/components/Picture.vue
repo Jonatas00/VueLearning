968cfd093e077155a625df5bb0a0eb825122d80c
Jonatas <djonatas31@gmail.com>
2023-08-03 17:22:51 -0300

    add

---
+++ 2projetoVue/src/components/Picture.vue
@@ -0,0 +1,16 @@
+<template>
+    <img v-bind:src="img" v-bind:alt="imgalt" v-bind:width="500">
+</template>
+
+<script>
+    export default {
+        name: 'Picture',
+        data () {
+            return {
+                img: '../public/img/img.jpeg',
+                imgalt: 'Luffynho',
+                imgwidth: '500'
+            }
+        }
+    }
+</script>
