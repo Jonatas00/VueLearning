4b4f860e648ad13dba4957746cb9278ba11f273f
Jonatas <djonatas31@gmail.com>
2023-08-03 17:12:00 -0300

    add v-bind

--- 2projetoVue/src/components/Info.vue
+++ 2projetoVue/src/components/Info.vue
@@ -1,16 +1,27 @@
 <template>
     <div>
-        <p>Estou trabalhando no momento.</p>
-        <p>Utilizo as seguintes tecnologias</p>
+        <p v-if="esta_trabalhando">Estou trabalhando no momento.</p>
+        <p v-else>Não estou trabalhando no momento</p>
+        <p>Utilizo as seguintes tecnologias:</p>
         <ul>
             <li>Javascript</li>
-            <li>PHP</li>
             <li>Python</li>
         </ul>
+
+        <p>Você pode visualizar meu GitHub <a v-bind:href="github" target="_blank">aqui</a></p>
+
     </div>
 </template>
 <script>
     export default {
-        name: 'Info'
+        name: 'Info',
+        data () {
+            return {
+                esta_trabalhando: false,
+                email: 'djonatas31@gmail.com',
+                mostrar_email: false,
+                github: "https://github.com/Jonatas00"
+            }
+        }
     }
 </script>
