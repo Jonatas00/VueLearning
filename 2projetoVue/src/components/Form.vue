4fdc6665dce2187055d7972cda7d5bc81ee84f19
Jonatas <djonatas31@gmail.com>
2023-08-04 12:49:59 -0300

    Events @submit and @click

--- 2projetoVue/src/components/Form.vue
+++ 2projetoVue/src/components/Form.vue
@@ -1,10 +1,10 @@
 <template>
-    <form action="">
+    <form action="" @submit="enviarFormulario($event)">
         <div>
-            <InputText />
+            <input type="text" v-model="name">
         </div>
         <div>
-            <InputText />
+            <input type="text" v-model="email">
         </div>
         <div>
             <Submit />
@@ -21,6 +21,24 @@
         components: {
             InputText,
             Submit
+        },
+        data() {
+            return {
+                name: '',
+                email: ''
+            }
+        },
+        methods: {
+            enviarFormulario(e) {
+                e.preventDefault()
+
+                const name = this.name;
+                const email = this.email;
+
+                console.log("Formulário enviado")
+                console.log("O nome é:", name)
+                console.log("O email é:", email)
+            }
         }
     }
 </script>
