3c21fc8860f5e8c3a1de31fb7df9b6062f07cc06
Jonatas <djonatas31@gmail.com>
2023-08-04 16:52:12 -0300

    add Reutilizacao

--- 2projetoVue/src/components/componente.vue
+++ 2projetoVue/src/components/componente.vue
@@ -3,16 +3,22 @@
         <h1>Olá, Vue!</h1>
         <p>Meu nome é {{ nome }} e eu sou um {{ profissao }}</p>
         <MultiplosEventos />
+
+        <Reutilizacao />
+        <Reutilizacao />
+        <Reutilizacao />
     </div>
 </template>
 
 <script>
     import MultiplosEventos from "./MultiplosEventos.vue"
+    import Reutilizacao from "./Reutilizacao.vue"
 
     export default {
         name: 'componente',
         components: {
-            MultiplosEventos
+            MultiplosEventos,
+            Reutilizacao
         },
         data() {
             return {
