c538558fab02649bd0f0ee4e7fbe665f20f26fdc
Jonatas <djonatas31@gmail.com>
2023-08-04 16:03:20 -0300

    add Multiplos Eventos

---
+++ 2projetoVue/src/components/MultiplosEventos.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <button @click="primeiro($event, 'Primeiro evento ativado!'), segundo($event, 'Segundo evento ativado!'), terceiro()">Ativar múltiplos eventos</button>
+    <p>{{ multiplos_eventos }}</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'MultiplosEventos',
+    data() {
+      return {
+        multiplos_eventos: ''
+      }
+    },
+    methods: {
+      primeiro(e, txt) {
+        this.multiplos_eventos = txt
+      },
+      segundo (e, txt) {
+        setTimeout(() => {this.multiplos_eventos = txt}, 1000)
+      },
+      terceiro(e, txt) {
+        setTimeout(() => {this.multiplos_eventos = ""}, 2000)
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  div {
+    margin: 100px;
+  }
+</style>
--- 2projetoVue/src/components/componente.vue
+++ 2projetoVue/src/components/componente.vue
@@ -2,12 +2,18 @@
     <div>
         <h1>Olá, Vue!</h1>
         <p>Meu nome é {{ nome }} e eu sou um {{ profissao }}</p>
+        <MultiplosEventos />
     </div>
 </template>
 
 <script>
+    import MultiplosEventos from "./MultiplosEventos.vue"
+
     export default {
         name: 'componente',
+        components: {
+            MultiplosEventos
+        },
         data() {
             return {
                 nome: "Jonatas",
