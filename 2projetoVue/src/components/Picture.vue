aca764bce12e56f64fca766ed45e03474a2a4538
Jonatas <djonatas31@gmail.com>
2023-08-05 16:51:42 -0300

    Como funciona o emit

--- 2projetoVue/src/components/Picture.vue
+++ 2projetoVue/src/components/Picture.vue
@@ -1,15 +1,30 @@
 <template>
-    <img :src="img" :alt="imgalt" :width="500">
+    <img :src="img" :alt="imgalt" :width="imgwidth">
+    <MudarImagem @mudar-imagem="TrocarImagem" />
 </template>
 
 <script>
+    import MudarImagem from './MudarImagem.vue';
+
     export default {
         name: 'Picture',
+        components: {
+            MudarImagem
+        },
         data () {
             return {
                 img: '/img/img01.png',
                 imgalt: 'LuffyG4',
                 imgwidth: '500'
+            }
+        },
+        methods: {
+            TrocarImagem() {
+                if (this.img != '/img/img02.jpeg') {
+                    this.img = '/img/img02.jpeg'
+                } else {
+                    this.img = 'img/img01.png'
+                }
             }
         }
     }
