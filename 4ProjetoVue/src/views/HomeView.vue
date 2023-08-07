241d97c986123fb2187f0956c8cdadfe67c63351
Jonatas <djonatas31@gmail.com>
2023-08-07 17:53:07 -0300

    Add Banner

--- 4ProjetoVue/src/views/HomeView.vue
+++ 4ProjetoVue/src/views/HomeView.vue
@@ -1,8 +1,19 @@
 <template>
-  <main class="main-container">
-    <h1>HOME</h1>
-  </main>
+  <Banner />
+  <div class="main-container">
+    <h1>Monte o seu burger:</h1>
+    <BurgerForm />
+  </div>
 </template>
 <script>
+  import Banner from '../components/Banner.vue';
+  import BurgerForm from '../components/BurgerForm.vue';
 
+  export default {
+    name: 'Home',
+    components: {
+      Banner,
+      BurgerForm
+    }
+  }
 </script>
